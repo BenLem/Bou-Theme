5c86d7dc81a242cf5940772bcabe1ac5ae1efb65
ben <ben.lemoine.bl@gmail.com>
2016-11-12 10:00:41 +0100

    Rend la barre de recherche fonctionnelle

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
         <footer class="main">
             <div class="content">
                 <p class="left"><?php echo site_meta('footer', site_description()); ?></p>
-                
-                <div class="search"><input type="search" placeholder="Rechercher" name="search"></div>
             
+                <form class="search" action="<?php echo search_url(); ?>" method="post">
+                    <input type="search" id="term" name="term" placeholder="RECHERCHER" value="<?php echo search_term(); ?>">
+                </form>
+
                 <p class="copyright">&copy; <?php echo date('Y'); ?> Jean-Yves Bou - <a href="<?php echo base_url(); ?>mentions-legales">Mentions légales</a> - Powered by <a href='http://anchorcms.com'>Anchor</a> - Thème par Benjamin Lemoine</p>
                 <a class='admin' href='<?php echo base_url();?>admin'>Administration</a>
             </div>
